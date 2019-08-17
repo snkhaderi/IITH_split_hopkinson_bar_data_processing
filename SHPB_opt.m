1db3c1b2dc338ea209aaedebbff640d4a2e45d94
snkhaderi <53366522+snkhaderi@users.noreply.github.com>
2019-08-17 18:37:23 +0530

    Update SHPB_opt.m

--- SHPB_opt.m
+++ SHPB_opt.m
@@ -8,7 +8,7 @@ function err = SHPB_opt(x)
     distance_ref = -x(1);
     distance_out = -x(2);
     [Dispersed_signal,stress_in,stress_out,e_rate,strain] = ...
-        postprocess_new(signal,distance_in,distance_ref,distance_out,C0,a,amp,Ebar,Values,f_cutt);
+        SHPB_process(signal,distance_in,distance_ref,distance_out,C0,a,amp,Ebar,Values,f_cutt);
     is = 1;                             % we can play with this parameter to enforce where to see eqbm
     ie = length(e_rate)*1;              % we can play with this parameter to enforce where to see eqbm
     err = norm(stress_in-stress_out);   % use this error measure for optimization
